e7334d7d4e6cccb322b9b1cac562aae3bdc7a267
alan <alan@alandeMacBook-Pro.local>
2015-09-16 10:27:20 +0800

    Initial Commit

---
+++ 许波波/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  许波波
+//
+//  Created by alan on 15/9/16.
+//  Copyright (c) 2015年 alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 许波波/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  许波波
+//
+//  Created by alan on 15/9/16.
+//  Copyright (c) 2015年 alan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
